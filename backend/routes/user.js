ca5e725ee06ef63ac87201f70dd73f287ccf6cd7
PrinceSachan <prinssachan41@gmail.com>
2024-03-22 16:30:11 +0530

    Feat: hashing password before saving into database

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt')
 const router = express.Router();
 
 const zod = require('zod')
@@ -31,9 +32,12 @@ router.post("/signup", async (req, res) => {
         })
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
+
     const user = await User.create({
         username: req.body.username,
-        password: req.body.password,
+        password: hashPassword,
         firstName: req.body.firstName,
         lastName: req.body.lastName
     })
@@ -63,35 +67,44 @@ const signInBody = zod.object({
 })
 
 router.post('/signin', async (req, res) => {
-    const { success } = signInBody.safeParse(req.body);
+    const { success } = await signInBody.safeParse(req.body);
     if(!success) {
-        res.status(411).json({
+        return res.status(411).json({
             message: 'Email already taken / Incorrect credentials'
         })
     }
 
-    const user = await User.findOne({
-        username: req.body.username,
-        password: req.body.password
-    })
+    try {
+        const user = await User.findOne({username: req.body.username});
+        if(!user){
+            return res.status(411).json({
+                message: 'User not found'
+            })
+        }
+        const comparePass = await bcrypt.compare(req.body.password, user.password);
+        if(!comparePass){
+            return res.status(400).json({
+                message: "Incorrect password"
+            })
+        }
+        
+        const userId = user._id;
 
-    const userId = user._id
-
-    if(user) {
         const token = jwt.sign({
             userId
-        }, process.env.JWT_SECRET)
-
+        }, process.env.JWT_SECRET);
+    
         res.json({
             token: token
-        })
-        return;
+        });
+    } catch(err) {
+        console.log('Error while fetching user info', err);
+        return res.status(500).json({
+            message: 'Error while logging in...'
+        });
     }
-
-    res.status(411).json({
-        message: 'Error while logging in...'
-    })
 })
+
 
 const updateBody = zod.object({
     username: zod.string().optional(),
